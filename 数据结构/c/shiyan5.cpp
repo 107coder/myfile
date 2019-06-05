7a35055c2630432102154b19c61095bd8f976d9d
107coder <1493255807@qq.com>
2019-06-05 21:37:32 +0800

    update shiyan5

--- 数据结构/c/shiyan5.cpp
+++ 数据结构/c/shiyan5.cpp
@@ -13,6 +13,12 @@ using namespace std;
 #define OVERFLOW -1
 #define FAIL -2;
 
+//显示提示信息
+void showMsg(string msg)
+{
+    cout << msg;
+}
+
 typedef char TElemType;
 typedef int Status;
 typedef int OtherInfo;
@@ -34,13 +40,15 @@ void showinfo()
 #define MVNum 100
 typedef char VerTexType;
 typedef int ArcType;
+//定于 邻接矩阵的一个总的结构
 typedef struct
 {
-    VerTexType vexs[MVNum];
-    ArcType arcs[MVNum][MVNum];
-    int vexnum,arcnum;
+    VerTexType vexs[MVNum];       //顶点表
+    ArcType arcs[MVNum][MVNum];    //顶点矩阵
+    int vexnum,arcnum;              //图的当前点数和边数
 }AMGraph;
 
+//定位某元素的位置，返回该位置的索引，若是没有找到，就直接返回FAIL（-2）
 Status LocateVex(AMGraph G,int v)
 {
     for(int i=0; i<G.vexnum; ++i)
@@ -49,13 +57,18 @@ Status LocateVex(AMGraph G,int v)
     }
     return FAIL;
 }
+//传入上面定义的图的邻接矩阵，并进行初始化和赋值
 Status CreateUDN(AMGraph &G)
 {
+    cout << "请输入邻接矩阵的顶点数 总边数（用空格隔开）：";
     cin >> G.vexnum >> G.arcnum;
+    cout << "请输入邻接矩阵的"<<G.vexnum<<"个顶点的信息（"<<G.vexnum<<"个数，用空格隔开）：";
     for(int i=0; i<G.vexnum; ++i)
     {
-        cin >> G.vexnum;
+        cout << "--"<<i;
+        cin >> G.vexs[i];
     }
+    //初始化邻接矩阵，把所有的值都置位无穷大
     for(int i=0; i<G.vexnum; ++i)
     {
         for(int j=0; j<G.vexnum; ++j)
@@ -64,9 +77,10 @@ Status CreateUDN(AMGraph &G)
         }
     }
 
-    for(int k=0; k<G.arcnum; ++k)
+    for(int k=0; k<G.arcnum; ++k)  //构造邻接矩阵
     {
         int v1,v2,w,i,j;
+        cout << "输入第"<<k+1<<"条边依附的顶点和权值：";
         cin >> v1 >> v2 >> w;
         i=LocateVex(G,v1);
         j=LocateVex(G,v2);
@@ -80,23 +94,23 @@ Status CreateUDN(AMGraph &G)
 //----------图的邻接表的存储结构-------
 
 #define MVNum 100
-typedef struct ArcNode              //变节点
+typedef struct ArcNode              //边节点
 {
     int adjvex;                     //该边所指向的顶点位置
     struct ArcNode * nextarc;       //指向下一条边的指针
     OtherInfo info;                 //和边相关的信息
 }ArcNode;
-typedef struct VNode
+typedef struct VNode                //顶点信息
 {
     VerTexType data;
-    ArcNode *firstarc;
-}VNode,AdjList[MVNum];
-typedef struct
+    ArcNode *firstarc;              //指向第一条依附顶点的指针
+}VNode,AdjList[MVNum];              //AdjList表示邻接表类型
+typedef struct                      //邻接表
 {
     AdjList vertices;
-    int vexnum,arcnum;
+    int vexnum,arcnum;              //图当前的顶点和边数
 }ALGraph;
-
+//定位表中元素的位置，返回索引值
 Status LocateVex(ALGraph G,int v)
 {
     for(int i=0; i<G.vexnum; ++i)
@@ -105,12 +119,13 @@ Status LocateVex(ALGraph G,int v)
     }
     return FAIL;
 }
-
+//使用邻接表创建图
 Status CreateUDG(ALGraph &G)
-{
-
+{//采用邻接表表示话，创建无向图G
+    cout << "请输入邻接矩阵的顶点数 总边数（用空格隔开）：";
     cin >> G.vexnum >> G.arcnum;
-    for(int i=0;i<G.vexnum; ++i)
+    cout << "请输入邻接矩阵的"<<G.vexnum<<"个顶点的信息（"<<G.vexnum<<"个数，用空格隔开）：";
+    for(int i=0;i<G.vexnum; ++i)        //输入各点，构造结点表头
     {
         cin >> G.vertices[i].data;
         G.vertices[i].firstarc = NULL;
@@ -139,8 +154,16 @@ Status CreateUDG(ALGraph &G)
 
 //-------深度优先遍历连通图------------
 bool visited[MVNum];             //访问标志数组，其处值为“false”
-void DFS(Graph G,int v)
-{//从第v个顶点出发递归地深度优先遍历图G
+int FirstAdjVex(ALGraph G,int v)
+{
+
+}
+int NextAdjVex(ALGraph G,int v,int w)
+{
+
+}
+void DFS(ALGraph G,int v)
+{ //从第v个顶点出发递归地深度优先遍历图G
     cout << v;
     visited[v]=true;            //访问第v个顶点，并置访问标志数组相应的分量为true
     for(int w=FirstAdjVex(G,v);w>0;w=NextAdjVex(G,v,w))
@@ -152,7 +175,7 @@ void DFS(Graph G,int v)
 }
 
 // ---------深度优先搜索遍历︿(￣︶￣)︿3非连通图
-void DFSTraverse(Graph G)
+void DFSTraverse(ALGraph G)
 {
 
 }
